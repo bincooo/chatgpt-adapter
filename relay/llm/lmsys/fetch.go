2b173c3bfd07aa899ceced434d34ff32a0b08ac1
bincooo <bingco.zn@gmail.com>
2025-05-30 15:50:40 +0800

    fix(lmsys): 修改域名地址

--- relay/llm/lmsys/fetch.go
+++ relay/llm/lmsys/fetch.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	baseUrl = "https://lmarena.ai"
+	baseUrl = "https://legacy.lmarena.ai"
 	ua      = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0"
 )
 
